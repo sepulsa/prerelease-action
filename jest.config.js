60e14a24f46c4531a38eb473291f418515be6b64
Abi <abi@alterra.id>
2021-04-04 13:29:55 +0700

    Debug context

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   clearMocks: true,
-  moduleFileExtensions: ['js', 'ts'],
+  moduleFileExtensions: ['js', 'json', 'ts'],
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   testRunner: 'jest-circus/runner',
