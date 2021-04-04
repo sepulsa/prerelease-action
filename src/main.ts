c8d676c9fd0567fe94cb93a5d90db6b2b8e03dcb
Abi <abi@alterra.id>
2021-04-04 13:29:55 +0700

    Debug context

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import {PullRequestClosedEvent} from '@octokit/webhooks-definitions/schema'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    if (github.context.eventName === 'pull_request') {
+      const payload = github.context.payload as PullRequestClosedEvent
+      // eslint-disable-next-line no-console
+      console.debug(payload.pull_request)
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
