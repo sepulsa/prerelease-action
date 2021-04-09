c94449d1f98fe0501f3183aeba240b1702b497d8
Abi <abi@alterra.id>
2021-04-09 09:19:58 +0700

    Use child_process

--- __tests__/git-tag.test.ts
+++ __tests__/git-tag.test.ts
@@ -1,16 +1,8 @@
-import {exec} from '@actions/exec'
+import {execSync} from 'child_process'
 import gitTag from '../src/git-tag'
 
-afterAll(async () => {
-  await exec('git', [
-    'tag',
-    '--delete',
-    '1.0.0',
-    '1.1.0-rc.0',
-    '1.1.0',
-    '1.2.0-rc.0',
-    'JIRA-999'
-  ])
+afterAll(() => {
+  execSync('git tag --list | xargs git tag --delete')
 })
 
 test('No previous tag', async () => {
@@ -18,10 +10,10 @@ test('No previous tag', async () => {
 })
 
 test('Increment semver', async () => {
-  await exec('git', ['tag', '1.0.0'])
-  await exec('git', ['tag', '1.1.0-rc.0'])
-  await exec('git', ['tag', '1.1.0'])
-  await exec('git', ['tag', '1.2.0-rc.0'])
-  await exec('git', ['tag', 'JIRA-999'])
+  execSync('git tag 1.0.0')
+  execSync('git tag 1.1.0-rc.0')
+  execSync('git tag 1.1.0')
+  execSync('git tag 1.2.0-rc.0')
+  execSync('git tag JIRA-999')
   expect(await gitTag()).toEqual('1.3.0-rc.0')
 })
